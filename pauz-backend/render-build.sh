0c1f3c036f0b9e15ae28057c82a59855c000a668
Elby2112 <loubna.bouzenzen2112@gmail.com>
2025-12-11 19:29:40 +0100

    fixing requirement.txt

--- pauz-backend/render-build.sh
+++ pauz-backend/render-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Update package list and install PortAudio dev libraries
 sudo apt-get update
 sudo apt-get install -y portaudio19-dev
+
+# Upgrade pip just in case
+pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
